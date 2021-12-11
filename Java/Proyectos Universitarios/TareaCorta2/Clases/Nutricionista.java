6f1902ce3099a048e7242feb89cf7a967eda305e
daperez2001 <88745647+daperez2001@users.noreply.github.com>
2021-12-11 10:49:51 -0600

    Delete Java directory

--- Java/Proyectos Universitarios/TareaCorta2/Clases/Nutricionista.java
+++
@@ -1,9 +0,0 @@
-/*Esta clase lo que hara es realizar la convercion para obtener el imc
-Autor:Daniel*/
-package Clases;
-	public class Nutricionista {
-
-		public double calcularImc(double pesoEnKilos, double alturaEnMetros){
-			return pesoEnKilos  / (alturaEnMetros * alturaEnMetros);
-		}
-	}
